2ed636e0588ffbbf07f97d966e989e75426b81da
Zhivka Dimova <ji.gancheva@gmail.com>
2019-04-16 16:55:37 +0200

    Reproduce problem using VueWordCloud with QSelect

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center" style="height: 100vh; width: 100wh;">
+    <vue-word-cloud :words="[['romance', 19], ['horror', 3], ['fantasy', 7], ['adventure', 3]]" />
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import VueWordCloud from 'vuewordcloud'
+
+export default {
+  components: {
+    [VueWordCloud.name]: VueWordCloud
+  },
+
+  name: 'PageIndex'
+}
+</script>
